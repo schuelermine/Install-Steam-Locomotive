e159f3c4c93d0e99ad95823dd5466af3bff48385
Anselm Schüler <anselmschueler@gmail.com>
2020-02-20 21:48:22 +0100

    fix wrogn things

--- Install-Steam-Locomotive.ps1
+++ Install-Steam-Locomotive.ps1
@@ -11,11 +11,8 @@ function Steam-Locomotive-Force {wsl sl}
 
 $HelpText = "This script helps you use the tremendous `"sl`" program in Windows PowerShell.
 Simply download the .ps1 file and execute it.
-
 If the script finishes successfully, you can type Steam-Locomotive in PS to start the interruptable sl -e. Use Steam-Locomotive-Force to prevent interruption (sl).
-
 Example:
-
 PS C:\Users\User\Downloads\> .\Install-Steam-Locomotive.ps1 -Profile $PROFILE -SkipReload
 Done!
 Success!
@@ -31,11 +28,11 @@ if (!$Profile) {
   $ErrorMessages += @("Please supply your profile location under -Profile. Cannot continue.")
 }
 
-if (Get-Command -Name "Steam-Locomotive" -and !$Force) {
+if ((Get-Command -Name "Steam-Locomotive") -and !$Force) {
   $ErrorMessages += @("It seems a command named `"Steam-Locomotive`" is already installed. Use -Force to continue anyways.")
 }
 
-if ((Get-Content $Profile | Select-String "Steam-Locomotive") -and $Profile) {
+if ((Get-Content $Profile | Select-String "Steam-Locomotive") -and $Profile -and !$Force) {
   $ErrorMessages += @("Your profile seems to already contain something called `"Steam-Locomotive`". Use -Force to continue anyways.")
 }
 
@@ -44,7 +41,7 @@ if (!(Get-Command -Name "wsl" -CommandType "Application")) {
 }
 
 if ($ErrorMessages) {
-  WriteOutput $ErrorMessages
+  Write-Output $ErrorMessages
   exit
 }
 
