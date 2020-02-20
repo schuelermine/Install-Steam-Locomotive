641946c024345ebfe338ac6120883b9be9ba1c0e
Anselm Schüler <anselmschueler@gmail.com>
2020-02-20 21:51:16 +0100

    remove reference to skipreload from help

--- Install-Steam-Locomotive.ps1
+++ Install-Steam-Locomotive.ps1
@@ -14,7 +14,7 @@ Simply download the .ps1 file and execute it.
 If the script finishes successfully, you can type Steam-Locomotive in PS to start the interruptable `"sl -e`".
 Use Steam-Locomotive-Force to prevent interruption (`"sl`").
 Example:
-PS C:\Users\User\Downloads\> .\Install-Steam-Locomotive.ps1 -Profile $PROFILE -SkipReload
+PS C:\Users\User\Downloads\> .\Install-Steam-Locomotive.ps1 -Profile $PROFILE
 "
 
 if ($Help) {
