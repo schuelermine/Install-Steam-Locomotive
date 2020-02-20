2a22f15ec529688fe165c274e315367b0b1af230
Anselm Schüler <anselmschueler@gmail.com>
2020-02-20 21:14:19 +0100

    remove skipreload and update help spacing

--- Install-Steam-Locomotive.ps1
+++ Install-Steam-Locomotive.ps1
@@ -1,18 +1,17 @@
-param([String]$Profile, [Switch]$SkipReload, [Switch]$Help)
+param([String]$Profile, [Switch]$Help)
 
 $HelpText = "This script helps you use the tremendous sl program in Windows PowerShell.
 Simply download the .ps1 file and execute it.
 
 If the script finishes successfully, you can type Steam-Locomotive in PS to start the interruptable sl -e. Use Steam-Locomotive-Force to prevent interruption (sl).
 
-If you don't want your profile to be reloaded, pass the -Skip-Reload switch.
-
 Example:
 
 PS C:\Users\User\Downloads\> .\Install-Steam-Locomotive.ps1 -Profile $PROFILE -SkipReload
 Done!
 Success!
-PS C:\Users\User\Downloads\> exit"
+PS C:\Users\User\Downloads\> exit
+"
 
 if ($Help) {
   Write-Output $HelpText
@@ -45,10 +44,6 @@ if (wsl command -v sl) {
 if (!(wsl command -v sl)) {
   Write-Output "Failed installing wsl."
   exit
-}
-
-if (!$SkipReload) {
-  .$Profile
 }
 
 Write-Output "Success!"
