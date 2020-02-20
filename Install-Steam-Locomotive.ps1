33b4d24dbc6e56cef8d40e40ff4a8e7ebba92e72
Anselm Schüler <anselmschueler@gmail.com>
2020-02-20 22:00:20 +0100

    fix error order and fix erroneous check for profile text

--- Install-Steam-Locomotive.ps1
+++ Install-Steam-Locomotive.ps1
@@ -22,20 +22,19 @@ if ($Help) {
   exit
 }
 
+if (!(Get-Command -Name "wsl" -CommandType "Application" -ErrorAction SilentlyContinue)) {
+  $ErrorMessages += @("You don't have WSL installed. Cannot continue.")
+}
+
 if (!$Profile) {
   $ErrorMessages += @("Please supply your profile location under -Profile. Cannot continue.")
+  if (Get-Content $Profile -ErrorAction SilentlyContinue | Select-String "Steam-Locomotive") {
+    $ErrorMessages += @("Your profile seems to already contain something called `"Steam-Locomotive`". Use -Force to continue anyways.")
+  }
 }
 
 if ((Get-Command -Name "Steam-Locomotive" -ErrorAction SilentlyContinue) -and !$Force) {
   $ErrorMessages += @("It seems a command named `"Steam-Locomotive`" is already installed. Use -Force to continue anyways.")
-}
-
-if ((Get-Content $Profile -ErrorAction SilentlyContinue | Select-String "Steam-Locomotive") -and $Profile -and !$Force) {
-  $ErrorMessages += @("Your profile seems to already contain something called `"Steam-Locomotive`". Use -Force to continue anyways.")
-}
-
-if (!(Get-Command -Name "wsl" -CommandType "Application" -ErrorAction SilentlyContinue)) {
-  $ErrorMessages += @("You don't have WSL installed. Cannot continue.")
 }
 
 if ($ErrorMessages) {
