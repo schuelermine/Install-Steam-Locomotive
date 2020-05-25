381c03536ed8e1889501a5324113be3f91cfebe0
Anselm Schüler <git@anselmschueler.com>
2020-05-25 19:34:55 +0200

    indent

--- Install-Steam-Locomotive.ps1
+++ Install-Steam-Locomotive.ps1
@@ -1,4 +1,8 @@
-param([String]$Profile, [Switch]$Help, [Switch]$Force)
+param(
+    [String]$Profile,
+    [Switch]$Help,
+    [Switch]$Force
+)
 
 $ErrorMessages = @()
 
@@ -18,42 +22,42 @@ PS C:\Users\User\Downloads\> .\Install-Steam-Locomotive.ps1 -Profile $PROFILE
 "
 
 if ($Help) {
-  Write-Output $HelpText
-  exit
+    Write-Output $HelpText
+    exit
 }
 
 if (-not (Get-Command -Name "wsl" -CommandType "Application" -ErrorAction SilentlyContinue)) {
-  $ErrorMessages += @("You don't have WSL installed. Cannot continue.")
+    $ErrorMessages += @("You don't have WSL installed. Cannot continue.")
 }
 
 if ([String]::IsNullOrEmpty($Profile)) {
-  $ErrorMessages += @("Please supply your profile location under -Profile. Cannot continue.")
+    $ErrorMessages += @("Please supply your profile location under -Profile. Cannot continue.")
 } elseif (Get-Content $Profile -ErrorAction SilentlyContinue | Select-String "Steam-Locomotive") {
-  $ErrorMessages += @("Your profile seems to already contain something called `"Steam-Locomotive`". Use -Force to continue anyways.")
+    $ErrorMessages += @("Your profile seems to already contain something called `"Steam-Locomotive`". Use -Force to continue anyways.")
 }
 
 if ((Get-Command -Name "Steam-Locomotive" -ErrorAction SilentlyContinue) -and !$Force) {
-  $ErrorMessages += @("It seems a command named `"Steam-Locomotive`" is already installed. Use -Force to continue anyways.")
+    $ErrorMessages += @("It seems a command named `"Steam-Locomotive`" is already installed. Use -Force to continue anyways.")
 }
 
 if ($ErrorMessages) {
-  Write-Output $ErrorMessages
-  exit
+    Write-Output $ErrorMessages
+    exit
 }
 
 if (-not (wsl command -v sl)) {
-  Write-Output "First, install SL."
-  wsl sudo apt install sl
+    Write-Output "First, install SL."
+    wsl sudo apt install sl
 }
 
 if (wsl command -v sl) {
-  Add-Content $Profile $Payload
-  Write-Output "Done!"
+    Add-Content $Profile $Payload
+    Write-Output "Done!"
 }
 
 if (-not (wsl command -v sl)) {
-  Write-Output "Failed installing wsl."
-  exit
+    Write-Output "Failed installing wsl."
+    exit
 }
 
 Write-Output "Success!"
